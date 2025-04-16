60cc7f8e7152e880c9f4c67a2e11129f3b32eabe
markaxelus <mrkaxelus@gmail.com>
2025-04-16 02:03:49 -0700

    Sidebar component

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from '@/app/(components)/Navbar'
+import Sidebar from '@/app/(components)/Sidebar'
 
 type Props = {
     children: React.ReactNode;
@@ -9,7 +10,7 @@ const DashboardWrapper = ({ children } : Props) => {
   return (
     <div className='flex min-h-screen w-full bg-gray-50 text-gray-900'>
         {/* Sidebar */}
-        Sidebar
+        <Sidebar />
         <main className={`flex w-full flex-col bg-gray-50 md:pl-64 dark:bg-dark-bg`}>
             {/* Navbar */}
             <Navbar />
