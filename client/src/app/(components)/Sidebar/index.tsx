19b8307dbb54a1401ce5dc9e32726caee6b03cf2
markaxelus <mrkaxelus@gmail.com>
2025-04-18 07:26:46 -0700

    sidebar styling

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -72,11 +72,16 @@ const SidebarLink = ({
     );
 
     return (
-        <Link href={href} className=''>
-            <div className="">
+        <Link href={href} className='w-full'>
+            <div className={`relative flex cursor-pointer items-center gap-3 transition-colors hover:bg-gray-100 dark:bg-black dark:hover:bg-gray-700 
+                            ${isActive ? "bg-gray-100 text-white dark:bg-gray-600" : ""}`}>
                 {isActive && 
-                    <div></div>
+                    <div className='absolute left-0 top-0 h-[100%] w-[5px] bg-blue-200'></div>
                 }
+                <Icon className='h-6 w-6 text-gray-800 dark:text-gray-100'/>
+                <span className={`font-medium text-gray-800 dark:text-gray-100`}>
+                    {label}
+                </span>
             </div>
         </Link>
     )
