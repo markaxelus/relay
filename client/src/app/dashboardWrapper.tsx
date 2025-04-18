478649b78edaff48c856a5b843d95a12abcafb37
markaxelus <mrkaxelus@gmail.com>
2025-04-17 19:11:05 -0700

    allows global state for client on wrapper

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,23 +1,51 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect } from 'react'
 import Navbar from '@/app/(components)/Navbar'
 import Sidebar from '@/app/(components)/Sidebar'
+import StoreProvider, { useAppSelector } from './redux'
 
 type Props = {
     children: React.ReactNode;
 }
 
+const DashboardLayout = ({ children } : Props) => {
+
+    const isSidebarCollapsed = useAppSelector(
+        (state) => state.global.isSidebarCollapsed
+    );
+    const isDarkMode = useAppSelector(
+        (state) => state.global.isDarkMode
+    );
+
+    useEffect(() => {
+        if(isDarkMode) {
+            document.documentElement.classList.add("dark");
+        } else {
+            document.documentElement.classList.remove("dark");
+        }
+    })
+
+    return (
+        <div className='flex min-h-screen w-full bg-gray-50 text-gray-900'>
+            {/* Sidebar */}
+            <Sidebar />
+            <main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg
+                ${isSidebarCollapsed ? "" : "md:pl-64"}`}>
+                {/* Navbar */}
+                <Navbar />
+                {children}
+            </main>
+        </div>
+    )
+}
+
 const DashboardWrapper = ({ children } : Props) => {
-  return (
-    <div className='flex min-h-screen w-full bg-gray-50 text-gray-900'>
-        {/* Sidebar */}
-        <Sidebar />
-        <main className={`flex w-full flex-col bg-gray-50 md:pl-64 dark:bg-dark-bg`}>
-            {/* Navbar */}
-            <Navbar />
-            {children}
-        </main>
-    </div>
-  )
+    return(
+        <StoreProvider>
+            <DashboardLayout>{children}</DashboardLayout>
+        </StoreProvider>
+    )
 }
 
 export default DashboardWrapper
