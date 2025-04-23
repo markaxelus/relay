d43e8b873f838f3c65ba7ab883116845af44d2fd
markaxelus <mrkaxelus@gmail.com>
2025-04-22 18:25:26 -0700

    redux setup & global state

---
+++ client/src/state/index.tsx
@@ -0,0 +1,27 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+export interface initialStateTypes {
+  isSidebarCollapsed: boolean;
+  isDarkMode: boolean;
+}
+
+const initialState = {
+  isSidebarCollapsed: false,
+  isDarkMode: false,
+}
+
+export const globalSlice = createSlice({
+  name: "global",
+  initialState,
+  reducers:({
+    setIsSidebarCollapsed: (state, action: PayloadAction<boolean>) => {
+      state.isSidebarCollapsed = action.payload;
+    },
+    setisDarkMode: (state, action: PayloadAction<boolean>) => {
+      state.isDarkMode = action.payload;
+    }
+  })
+})
+
+export const {} = globalSlice.actions;
+export default globalSlice.reducer;
