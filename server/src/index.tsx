9c99d8f0f85c8da1564fde86084805e3ee6ff133
markaxelus <mrkaxelus@gmail.com>
2025-04-24 23:08:21 -0700

    server setup for backend

---
+++ server/src/index.tsx
@@ -0,0 +1,30 @@
+import express from "express"
+import dotenv from "dotenv"
+import bodyParser from "body-parser"
+import cors from "cors"
+import helmet from "helmet"
+import morgan from "morgan"
+
+dotenv.config();
+const port = process.env.PORT || 3000;
+
+const app = express();
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan("common"));
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(cors());
+
+
+
+/* Routes */
+app.get('/', (req, res) => {
+  res.send("Home Route");
+})
+
+
+app.listen(port, () => {
+  console.log(`Server running on port: http://localhost:${port}`)
+})
