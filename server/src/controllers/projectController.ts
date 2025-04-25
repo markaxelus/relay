e1a2617055e651cd7425aec3c4981fdc2bbc398d
markaxelus <mrkaxelus@gmail.com>
2025-04-24 23:14:36 -0700

    projectController to create and get project(s)

--- server/src/controllers/projectController.ts
+++ server/src/controllers/projectController.ts
@@ -1,1 +1,35 @@
-/* getProjects, createProject */
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const getProjects = async(
+  req: Request,
+  res: Response,
+) : Promise<void> => {
+  try {
+    res.json(await prisma.project.findMany());
+  } catch (error: any) {
+    res.status(500).json({ message: `Error retrieving projects: ${error.message}` })
+  }
+}
+
+export const createProjects = async(
+  req: Request,
+  res: Response,
+) : Promise<void> => {
+  const { name, description, startDate, endDate } = req.body;
+  try {
+    const newProject = await prisma.project.create({
+      data: {
+        name,
+        description,
+        startDate,
+        endDate
+      }
+    })
+    res.status(201).json(newProject);
+  } catch (error: any) {
+    res.status(500).json({ message: `Error creating project: ${error.message}` });
+  }
+}
