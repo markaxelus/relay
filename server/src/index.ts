906d116df390130e8a6bc25ba811a44347d30b2b
markaxelus <mrkaxelus@gmail.com>
2025-04-24 23:22:09 -0700

    routes setup for projects

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from "body-parser"
 import cors from "cors"
 import helmet from "helmet"
 import morgan from "morgan"
+import projectRoutes from "./routes/projectRoutes"
 
 dotenv.config();
 const port = process.env.PORT || 3000;
@@ -23,6 +24,8 @@ app.use(cors());
 app.get('/', (req, res) => {
   res.send("Home Route");
 })
+
+app.use('/projects', projectRoutes);
 
 
 app.listen(port, () => {
