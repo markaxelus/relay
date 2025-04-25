f614368b49660e76ef662a79aa690009c8ee94fb
markaxelus <mrkaxelus@gmail.com>
2025-04-24 21:47:18 -0700

    seeded DB

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -8,7 +8,7 @@ async function deleteAll (orderedFileNames: string[]) {
   const allModel = orderedFileNames.map((filename) => {
       const extName = path.extname(filename);
       const modelName = path.basename(filename, extName);
-      return modelName.charAt(0).toUpperCase + modelName.slice(1);
+      return modelName.charAt(0).toUpperCase() + modelName.slice(1);
   })
 
 
@@ -16,20 +16,33 @@ async function deleteAll (orderedFileNames: string[]) {
     const model: any = prisma[models as keyof typeof prisma];
     try {
       await model.deleteMany({});
-      console.log(`Successfully deleted data from ${model}`);
+      console.log(`Successfully deleted data from ${models}`);
     } catch (error) {
-      console.error(`Error deleting data from ${model}: `, error);
+      console.error(`Error deleting data from ${models}: `, error);
     }
   }
-
 }
 
-async function insertAll (orderedFileNames: string[]) {
+async function insertAll (dataPathName: string, orderedFileNames: string[]) {
+  for (const fileName of orderedFileNames) {
+    const filePathOfData = path.join(dataPathName, fileName);
+    const jsonData = JSON.parse(fs.readFileSync(filePathOfData, "utf-8"));
+    const modelName = path.basename(fileName, path.extname(fileName));
+    const model: any = prisma[modelName as keyof typeof prisma];
 
+    try {
+      for (const data of jsonData) {
+        await model.create({ data });
+      }
+      console.log(`Successfully seeded ${modelName} from ${fileName}`);
+    } catch (error) {
+      console.error(`Error seeding data for ${modelName}:`, error);
+    }
+  }
 }
 
 async function main() {
-  const pathName = path.join(__dirname, "data");
+  const dataPathName = path.join(__dirname, "data");
 
   const orderedFileNames = [
     "team.json",
@@ -42,5 +55,10 @@ async function main() {
     "taskAssignment.json",
   ];
 
-
+  await deleteAll(orderedFileNames);
+  await insertAll(dataPathName, orderedFileNames);
 }
+
+main()
+  .catch((error) => console.error(error))
+  .finally(async() => await prisma.$disconnect());
