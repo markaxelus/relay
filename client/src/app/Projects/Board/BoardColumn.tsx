60579b228f1a4628b28ae207f9e73a92bed2261e
markaxelus <mrkaxelus@gmail.com>
2025-05-06 22:28:11 -0700

    status header for tasks

--- client/src/app/Projects/Board/BoardColumn.tsx
+++ client/src/app/Projects/Board/BoardColumn.tsx
@@ -24,28 +24,52 @@ const BoardColumn = ({
       moveTask(item.id, status);
     },
     collect: (m) => ({ isOver: !!m.isOver() }),
-  }))
-
+  }));
+  /* DEBUG STMTS
   console.log("Rendering column:", status);
   console.log(
-  tasks.map((t) => ({ id: t.id, status: t.status }))
-);
+  tasks.map((t) => ({ id: t.id, status: t.status })) 
+);*/
+  const tasksCount = tasks.filter((task) => task.status === status).length;
 
+  const statusColor: any = {
+    "To Do": "#2563EB",
+    "Work In Progress": "#059669",
+    "Under Review": "#D97706",
+    "Completed": "#000000",
+  };
 
   return (
-    
-    <div>
+    <div
+    ref={(instance) => {
+      drop(instance);
+    }}
+    className={`sl:py-4 rounded-lg py-2 xl:px-2 ${isOver ? "bg-blue-100 dark:bg-neutral-950" : ""}`}
+    >
+      
+      <div className="mb-3 flex w-full">
+        <div
+          className={`w-2 !bg-[${statusColor[status]}] rounded-s-lg`}
+          style={{ backgroundColor: statusColor[status] }}
+        />
+        <div className="a">
+          <h3 className="a">
+            {status}
+            <span className="a">
+              {tasksCount}
+            </span>
+          </h3>
+        </div>
+
+        
+      </div>
+
       {tasks
         .filter((task) => task.status === status)
         .map((task) => (
-          <TaskControl
-            key={task.id}
-            task={task}
-          />
+          <TaskControl key={task.id} task={task} />
         ))}
-        
     </div>
-      
   );
 };
 
