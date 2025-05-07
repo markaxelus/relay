92cb114ea312628627bc9e006b29f2113b7e5b5c
markaxelus <mrkaxelus@gmail.com>
2025-05-06 22:11:05 -0700

    some layout changes

--- client/src/app/Projects/Board/TaskControl.tsx
+++ client/src/app/Projects/Board/TaskControl.tsx
@@ -71,6 +71,35 @@ const TaskControl = ({ task }: TaskProps) => {
 
         {/* Task Info Container */}
         <div className="flex items-start justify-between">
+          
+          {/* Task Title */}
+          <h4 className="text-md font-bold dark:text-white">
+            {task.title}
+          </h4>
+          <button className="flex h-6 w-6 flex-shrink-0 items-center justify-center dark:text-neutral-500">
+            <EllipsisVertical size={30} />
+          </button>
+        </div>
+
+        <div className="my-3 flex justify-between">
+          {/* Task Points */}
+          <p className="text-sm text-gray-600 dark:text-neutral-500">
+            {task.description}
+          </p>
+          {typeof task.points === "number" && (
+            <div className="text-xs font-semibold dark:text-white">
+              {task.points} pts
+            </div>
+          )}
+          {/* Task Date */}
+          {/* <div className="text-xs text-gray-500 dark:text-neutral-500">
+            {formatStartDate && <span>{formatStartDate} - </span>}
+            {formatDueDate && <span>{formatDueDate}</span>}
+          </div> */}
+        </div>
+
+        {/* Task Priority Tags */}
+        <div>
           <div className="flex flex-1 flex-wrap items-center gap-2">
             {task.priority && <PriorityTag priority={task.priority} />}
             <div className="flex gap-2">
@@ -85,37 +114,40 @@ const TaskControl = ({ task }: TaskProps) => {
               ))}
             </div>
           </div>
-          <button className="flex h-6 w-4 flex-shrink-0 items-center justify-center dark:text-neutral-500">
-            <EllipsisVertical size={26} />
-          </button>
-        </div>
-
-        <div className="my-3 flex justify-between">
-          {/* Task Title */}
-          <h4 className="text-md font-bold dark:text-white">
-            {task.title}
-          </h4>
-          {/* Task Points */}
-          {typeof task.points === "number" && (
-            <div className="text-xs font-semibold dark:text-white">
-              {task.points} pts
-            </div>
-          )}
-          {/* Task Date */}
-          <div className="text-xs text-gray-500 dark:text-neutral-500">
-            {formatStartDate && <span>{formatStartDate} - </span>}
-            {formatDueDate && <span>{formatDueDate}</span>}
-          </div>
-        </div>
-
-        {/* Task Description */}
-        <div>
-          <p className="text-sm text-gray-600 dark:text-neutral-500">
-            {task.description}
-          </p>
         </div>
         
         {/* Users */}
+        <div className="flex mt-3 items-center justify-between">
+          <div className="flex -space-x-[6px] overflow-hidden">
+            {task.assignee && (
+              <Image
+                key={task.assignedUserId}
+                src={`/${task.assignee.profilePictureUrl!}`}
+                alt={task.assignee.username}
+                width={30}
+                height={30}
+                className="dark:border-dark-secondary h-8 w-8 rounded-full border-2 border-white object-cover"
+              />
+            )}
+            {task.author && (
+              <Image
+              key={task.author.userId}
+              src={`/${task.author.profilePictureUrl!}`}
+              alt={task.author.username}
+              width={30}
+              height={30}
+              className="dark:border-dark-secondary h-8 w-8 rounded-full border-2 border-white object-cover"
+            />
+            )}
+          </div>
+          <div className="a">
+            <MessageSquareMore size={20} />
+            <span className="ml-1 text-sm dark:text-neutral-400">
+              {commentsAmount}
+            </span>
+          </div>
+        </div>
+
       </div>
 
     </div>
